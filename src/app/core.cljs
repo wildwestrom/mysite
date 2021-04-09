6c47e6df3620351916237d428ef0e7e904eddb6b
Christian Westrom <c.westrom@westrom.xyz>
2021-04-09 15:35:37 -0400

    updated the site

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -3,15 +3,27 @@
             [reagent.dom :as dom]))
 
 (def body-class
-  "dark:bg-gray-700")
+  "dark:bg-gray-900")
 
 (defn app []
-  [:div {:class '[m-6 p-6 dark:text-gray-200]}
-   [:h1 {:class '[text-5xl pb-2]} "Hello, World!"]
-   [:h2 {:class '[text-3xl py-2]} "What do we have here?"]
-   [:p "Looks like some fine styles here!"]
-   [:p "Welcome to my site!"]
-   ])
+  [:div {:class '[dark:text-gray-300 flex flex-col
+                  h-screen justify-between]}
+   [:div {:id    "content"
+          :class '[flex-1 text-left m-4 p-4 mb-auto]}
+    [:h1 {:class '[text-5xl pb-2]} "Hello, World!"]
+    [:h2 {:class '[text-3xl py-2]} "What do we have here?"]
+    [:p "Looks like some fine styles here!"]
+    [:p "Welcome to my site!"]]
+   [:footer {:class '[text-xs text-center w-full p-4]}
+    [:p "My "
+     [:a {:href  "https://gitlab.com/wildwestrom/cljs-tailwind-static"
+          :class '[text-blue-500 visited:text-purple-500]}
+      "static site generator"]
+     " is licensed under the "
+     [:a {:href  "https://www.gnu.org/licenses/agpl-3.0.html"
+          :class '[text-blue-500 visited:text-purple-500]} "AGPL license"] "."
+     [:br] "The rest is my own original work unless otherwise specified."
+     [:br] "Copyright © 2021 Christian Westrom c.westrom@westrom.xyz"]]])
 
 (defn mount-root [c]
   (dom/render [c]
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     ],
     darkMode: 'media', // or 'media' or 'class'
     theme: {
-        extend: {}
+        extend: {
+            textColor: ['visited'],
+        }
     },
     variants: {},
     plugins: []
