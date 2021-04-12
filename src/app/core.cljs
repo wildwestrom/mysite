2e3eee9ce04848a6bdd176927efb3447be07e2dc
Christian Westrom <c.westrom@westrom.xyz>
2021-04-12 02:21:35 -0400

    fixed the footer

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -5,29 +5,34 @@
   '[dark:bg-gray-900 dark:text-gray-300])
 
 (defn app []
-  [:div {:class '[flex flex-col h-screen
-                  justify-between p-6]}
+  [:div {:class '[flex flex-col
+                  justify-between px-6 pt-4 pb-2]}
+   [:div {:class '[h-screen]}
+    [:header {:class '[text-lg italic underline font-serif]}
+     [:h1 "West's Super Awesome Homepage!"]]
+    [:div {:id    "content"
+           :class '[flex-1 mb-auto px-5 pr-18]}
 
-   [:header {:class '[text-lg italic underline font-serif]}
-    [:h1 "West's Super Awesome Homepage!"]]
+     [:h2 {:class '[text-3xl py-2 italic]} "Welcome, to my site!"]
+     [:p {:class '[font-mono]}"Pretty soon you're gonna see my blog and other goodies.
+Trust me, you're gonna love it. A'ight? A'ight."]]]
 
-   [:div {:id    "content"
-          :class '[flex-1 mb-auto px-5 pr-18]}
-
-    [:h2 {:class '[text-3xl py-2 italic]} "Welcome, to my site!"]
-    [:p {:class '[font-mono]}"Pretty soon you're gonna see my blog and other goodies.
-Trust me, you're gonna love it. A'ight? A'ight."]]
-
-   [:footer {:class '[text-xs text-center p-2]}
-    [:p "My "
-     [:a {:href  "https://gitlab.com/wildwestrom/cljs-tailwind-static"
-          :class '[text-blue-500 visited:text-purple-500]}
-      "static site generator"]
-     " is licensed under the "
-     [:a {:href  "https://www.gnu.org/licenses/agpl-3.0.html"
-          :class '[text-blue-500 visited:text-purple-500]} "GNU AGPL license"] ". " [:br]
-     "The rest is my own original work unless otherwise specified. " [:br]
-     "Copyright © 2021 Christian Westrom c.westrom@westrom.xyz"]]])
+   [:footer {:class '[text-xs text-center]}
+    [:p
+     [:span.inline-block "My "
+      [:a {:href   "https://gitlab.com/wildwestrom/cljs-tailwind-static"
+           :target "_blank"
+           :class  '[text-blue-500 visited:text-purple-500]}
+       "static site generator"]
+      " is licensed"] " "
+     [:span.inline-block "under the "
+      [:a {:href   "https://www.gnu.org/licenses/agpl-3.0.html"
+           :target "_blank"
+           :class  '[text-blue-500 visited:text-purple-500]} "GNU AGPL license"] "."] [:br]
+     [:span.inline-block "The rest is my own original work"] " "
+     [:span.inline-block "unless otherwise specified."] [:br]
+     [:span.inline-block "Copyright © 2021 Christian Westrom "] " "
+     [:span.inline-block "c.westrom@westrom.xyz"]]]])
 
 (defn mount-root [c]
   (dom/render [c]
