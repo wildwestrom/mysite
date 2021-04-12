5393fef03bb649dabdb23a8baa92b7b4c55ce0fd
Christian Westrom <c.westrom@westrom.xyz>
2021-04-12 00:27:08 -0400

    major update to the homepage

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,28 +1,32 @@
 (ns app.core
   (:require [reagent.dom :as dom]))
 
-(def body-class
-  "dark:bg-gray-900")
+(def body-classes
+  '[dark:bg-gray-900 dark:text-gray-300])
 
 (defn app []
-  [:div {:class '[dark:text-gray-300 flex flex-col
-                  h-screen justify-between]}
+  [:div {:class '[flex flex-col h-screen
+                  justify-between p-6]}
+
+   [:header {:class '[text-lg italic underline font-serif]} "West's Super Awesome Homepage!"]
+
    [:div {:id    "content"
-          :class '[flex-1 text-left m-4 p-4 mb-auto]}
-    [:h1 {:class '[text-5xl pb-2]} "Hello, World!"]
-    [:h2 {:class '[text-3xl py-2]} "What do we have here?"]
-    [:p "Looks like some fine styles here!"]
-    [:p "Welcome to my site!"]]
-   [:footer {:class '[text-xs text-center w-full p-2]}
+          :class '[flex-1 mb-auto px-5 pr-18]}
+
+    [:h1 {:class '[text-3xl py-2 italic]} "Welcome, to my site!"]
+    [:p {:class '[font-mono]}"Pretty soon you're gonna see my blog and other goodies.
+Trust me, you're gonna love it. A'ight? A'ight."]]
+
+   [:footer {:class '[text-xs text-center p-2]}
     [:p "My "
      [:a {:href  "https://gitlab.com/wildwestrom/cljs-tailwind-static"
           :class '[text-blue-500 visited:text-purple-500]}
       "static site generator"]
      " is licensed under the "
      [:a {:href  "https://www.gnu.org/licenses/agpl-3.0.html"
-          :class '[text-blue-500 visited:text-purple-500]} "AGPL license"] "."
-     [:br] "The rest is my own original work unless otherwise specified."
-     [:br] "Copyright © 2021 Christian Westrom c.westrom@westrom.xyz"]]])
+          :class '[text-blue-500 visited:text-purple-500]} "AGPL license"] ". " [:wbr]
+     "The rest is my own original work unless otherwise specified. " [:wbr]
+     "Copyright © 2021 Christian Westrom c.westrom@westrom.xyz"]]])
 
 (defn mount-root [c]
   (dom/render [c]
@@ -34,7 +38,9 @@
 
 (defn ^:export init []
   (js/console.log "init")
-  (.classList.add (.getElementById js/document "body") body-class)
+  (doseq [class body-classes]
+    (.classList.add
+      (.getElementById js/document "body") class))
   (start))
 
 (defn ^:dev/before-load stop []
