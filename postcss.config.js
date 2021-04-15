4356359c38d674bb778fae7d4df4211ced0e4a2c
Christian Westrom <c.westrom@westrom.xyz>
2021-04-14 20:45:07 -0400

    formatting + added index.clj to postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,19 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-    isProduction && require('@fullhuman/postcss-purgecss')({
-      content: [
-        './src/app/core.cljs',
-      ],
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-    }),
-    isProduction && require('cssnano')({
-      preset: 'default',
-    }),
-  ],
+    plugins: [
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+        isProduction && require('@fullhuman/postcss-purgecss')({
+            content: [
+                './src/app/core.cljs',
+                './src/app/index.clj',
+            ],
+            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+        }),
+        isProduction && require('cssnano')({
+            preset: 'default',
+        }),
+    ],
 };
