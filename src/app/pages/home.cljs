92467d4994eb06c279d17c21d86d0fd42c97df8c
Christian Westrom <c.westrom@westrom.xyz>
2022-01-17 06:25:43 -0500

    changes to css mostly

--- src/app/pages/home.cljs
+++ src/app/pages/home.cljs
@@ -18,38 +18,41 @@
 (defn home-page
   []
   [:<>
-   [section-header "About"]
-   [:p "Over this past year I taught myself Clojure and ClojureScript, and soon I'll start working as a developer full-time. "]
-   [section-header "Contact"
-    [:<>
-     [:p "You can check out my " [common/generic-link (rfe/href :app.data.router/portfolio) "portfolio" :alt-target "_self"] "."]
-     [:p  "If you like what you see, please don't hesitate to send me a message."]]]
-   [:ul
-    [common/icon-link (:email global-config)
-     faEnvelope
-     "Email address"
-     :href (str "mailto:" (:email global-config))]
-    [common/icon-link "wildwestrom"
-     faGithub
-     "Github"
-     :href (:github global-config)]
-    [common/icon-link "c-westrom"
-     faLinkedin
-     "Linkedin"
-     :href (:linkedin global-config)]
-    [common/icon-link (:discord global-config)
-     faDiscord
-     "Discord"
-     :copyable true]
-    [common/icon-link "Public PGP Key"
-     faKey
-     "Public PGP Key"
-     :href (:pgp global-config)
-     :styles "mt-4"]]
-   [section-header "Fund"
-    "Coding is hard work; This helps me keep going."]
-   [:ul
-    [common/icon-link "Monero Wallet"
-     faMonero
-     "Monero wallet"
-     :href (str "monero:" (:monero global-config))]]])
+   [:div.about-section
+    [section-header "About"]
+    [:p "Over this past year I taught myself Clojure and ClojureScript." [:br] "Soon I'll start working as a developer full-time. "]]
+   [:div.about-section
+    [section-header "Contact"
+     [:<>
+      [:p "You can check out my " [common/generic-link (rfe/href :app.data.router/portfolio) "portfolio" :alt-target "_self"] "."]
+      [:p  "If you like what you see, please don't hesitate to send me a message."]]]
+    [:ul
+     [common/icon-link (:email global-config)
+      faEnvelope
+      "Email address"
+      :href (str "mailto:" (:email global-config))]
+     [common/icon-link "wildwestrom"
+      faGithub
+      "Github"
+      :href (:github global-config)]
+     [common/icon-link "c-westrom"
+      faLinkedin
+      "Linkedin"
+      :href (:linkedin global-config)]
+     [common/icon-link (:discord global-config)
+      faDiscord
+      "Discord"
+      :copyable true]
+     [common/icon-link "Public PGP Key"
+      faKey
+      "Public PGP Key"
+      :href (:pgp global-config)
+      :styles "mt-4"]]]
+   [:div.about-section
+    [section-header "Fund"
+     "Coding is hard work; This helps me keep going."]
+    [:ul
+     [common/icon-link "Monero Wallet"
+      faMonero
+      "Monero wallet"
+      :href (str "monero:" (:monero global-config))]]]])
