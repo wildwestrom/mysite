0f2389a66bfc8ba0dbd55aaca20f4ce62e258602
Christian Westrom <c.westrom@westrom.xyz>
2022-11-04 04:41:05 -0400

    update homepage and readme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,16 @@
+<script lang="ts">
+	import ExternalLink from '$lib/ExternalLink.svelte';
+</script>
+
 <svelte:head>
 	<title>Westrom.xyz - Home</title>
 </svelte:head>
 
 <h1>Welcome to my site!</h1>
+
+<p>
+	Right now there's not much here. If you wanna give me suggestions, submit an
+	issue or pull request via <ExternalLink
+		link="https://github.com/wildwestrom/mysite">GitHub</ExternalLink
+	>.
+</p>
