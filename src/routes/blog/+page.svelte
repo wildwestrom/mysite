afc4859f4cf2855f0475de5d95ad473f57d02abd
West <c.westrom@westrom.xyz>
2022-11-10 02:11:14 +0000

    Fix Blog Post Preview Page Styling/A11y (#23)
    * attempt #1
    
    * keep list of blog posts an unordered list, since I don't display numbers
    
    * add specific styles to make bottom border on every item except last
    
    * make svelte-check happy

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import BlogPostPreviewCard from './BlogPostPreviewCard.svelte';
 	import type { PageData } from './$types';
 	export let data: PageData;
+
 </script>
 
 <svelte:head>
@@ -10,11 +10,82 @@
 
 <h1>Blog</h1>
 <div>
-	<ol>
-		{#each data.posts as post}
+	<ul class="blogposts">
+		{#each data.posts as blogpost}
 			<li>
-				<BlogPostPreviewCard blogpost={post} />
+				<div class="preview-container">
+					<a href="/blog/{blogpost.filePath}">
+						<h2>{blogpost.title}</h2>
+						<p class="subtitle color-2">{blogpost.subtitle}</p>
+					</a>
+					{#if blogpost.tags}
+						<ul class="tags color-2">
+							{#each blogpost.tags.split(' ') as tag}
+								<li class="tag">
+									<a href="/blog/?tag={tag}">{tag}</a>
+								</li>
+							{/each}
+						</ul>
+					{:else}
+						No tags
+					{/if}
+				</div>
 			</li>
 		{/each}
-	</ol>
+	</ul>
 </div>
+
+<style lang="postcss">
+	ul.blogposts > li:last-child .preview-container {
+		border: none;
+	}
+
+	.preview-container {
+		font-size: 0.8rem;
+		padding-bottom: 1em;
+		margin: 0.5em;
+		line-height: 1.1;
+		border-color: var(--text-color);
+		border-bottom-width: 1px;
+		margin-bottom: 0.75em;
+	}
+
+	a {
+		color: hsl(235 80% 60%);
+		p {
+			padding-bottom: .75em;
+		}
+	}
+
+	h2 {
+		font-size: 1rem;
+		font-weight: 400;
+		text-decoration: underline;
+		padding-bottom: .75em;
+	}
+
+	h2:hover {
+		text-decoration: none;
+	}
+
+	.color-2 {
+		color: var(--text-secondary);
+		border-color: var(--text-secondary);
+	}
+
+	.tags {
+		display: inline-flex;
+		flex-wrap: wrap;
+		padding-bottom: 0;
+	}
+
+	.tag {
+		margin: 0.2rem;
+		border-style: solid;
+		border-width: 1px;
+		border-radius: 0.25em;
+		padding-left: 0.5em;
+		padding-right: 0.5em;
+		padding: 0.25rem;
+	}
+</style>
