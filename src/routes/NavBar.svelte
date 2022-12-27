a3b9938d74deb36b1ea728b2258b471aeb53830f
Christian Westrom <c.westrom@westrom.xyz>
2022-12-28 07:15:00 +0900

    remove WIP hamburger menu

--- src/routes/NavBar.svelte
+++ src/routes/NavBar.svelte
@@ -1,12 +1,5 @@
 <nav class="border-current">
-	<button class="hamburger">
-		<svg viewBox="0 0 100 100" stroke="currentColor" stroke-width="10" stroke-linecap="round">
-			<line x1="10" x2="90" y1="20" y2="20" />
-			<line x1="10" x2="90" y1="50" y2="50" />
-			<line x1="10" x2="90" y1="80" y2="80" />
-		</svg>
-	</button>
-	<ul class="nav-items">
+	<ul id="primary-navigation">
 		<li>
 			<a href="/">Home</a>
 		</li>
@@ -23,37 +16,6 @@
 </nav>
 
 <style>
-	.hamburger {
-		background-color: inherit;
-		padding: 0.75em;
-		width: 4rem;
-		height: 4rem;
-		border-width: 0;
-		display: none;
-		color: var(--text-color);
-
-		@media (max-width: 639px) {
-			display: block;
-			padding: 0;
-			&:hover svg {
-				line {
-					y1: 50;
-					y2: 50;
-				}
-			}
-		}
-	}
-
-	@media (max-width: 639px) {
-		.nav-items {
-			display: none;
-		}
-
-		:hover {
-			display: block;
-		}
-	}
-
 	nav {
 		background-color: var(--bg-secondary);
 		display: flex;
@@ -61,26 +23,12 @@
 		justify-content: space-between;
 		margin-bottom: 1rem;
 		padding: 0.75em;
-
-		@media (max-width: 639px) {
-			position: fixed;
-			bottom: 2rem;
-			left: 0;
-			right: 0;
-			z-index: 5;
-			max-width: fit-content;
-			margin: auto;
-			border-radius: 0.5rem;
-
-			:hover {
-				border-radius: 0.5rem;
-			}
-		}
 	}
 
-	ul {
+	#primary-navigation {
 		display: flex;
 		flex-wrap: wrap;
+		align-items: center;
 		row-gap: 0.5rem;
 		column-gap: 2rem;
 		margin: auto;
